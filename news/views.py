1ac191eecbba6a2220f3bb54feb94603959fbda7
damunza <danidamunza@gmail.com>
2018-09-24 14:01:06 +0300

    displayed data from a template

--- news/views.py
+++ news/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import  HttpResponse, Http404
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
 
 def news_of_day(request):
     date = dt.date.today()
@@ -28,4 +28,24 @@ def convert_dates(dates):
      day = days[day_number]
      return  day
 
+def past_days_news(request, past_date):
 
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # raise 404 when value is thrown
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+            <html>
+                <body>
+                    <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+                </body>
+            </html>
+                '''
+
+
+    return HttpResponse(html)
