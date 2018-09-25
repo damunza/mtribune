1d25e940ab5aa7bfd5ac00c3f65abfed242d5a71
damunza <danidamunza@gmail.com>
2018-09-25 12:04:24 +0300

    implemented the search include action on the template

--- news/views.py
+++ news/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import  HttpResponse, Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
 
 
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()
+    news = Article.todays_news()
 
-    return render(request,'all-news/today-news.html',{'date': date,})
+    return render(request,'all-news/today-news.html',{'date': date,'news': news})
 
 def past_days_news(request, past_date):
 
@@ -22,7 +24,21 @@ def past_days_news(request, past_date):
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
 
+    news = Article.days_news(date)
 
-    return render(request,'all-news/past-news.html',{'date': date,})
+    return render(request,'all-news/past-news.html',{'date': date,'news': news})
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
