e3ec415c59c583ec5bd4f34bc678d962db6e2f0e
damunza <danidamunza@gmail.com>
2018-09-26 14:47:44 +0300

    added a feature to allow users to add their own articles

--- news/models.py
+++ news/models.py
@@ -14,7 +14,7 @@ class tags(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length =60)
-    post = models.TextField()
+    post = HTMLField()
     editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
